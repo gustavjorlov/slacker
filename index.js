8d857cb87c9b295eb921f54b8d541974cb091c40
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-06 15:01:00 +0100

    [id-123] slack integration 2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
+const core = require("@actions/core");
 
 const slackUrl = core.getInput("slack-url");
 const message = core.getInput("message");
 
 try {
   axios.post(slackUrl, {
-    text: message
+    text: `[PR ${github.context.payload.pull_request.head.ref}]: ${message}`
   });
 } catch (error) {
   core.setFailed(error.message);
