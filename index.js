1d17481c2943f8c4eb8dc28bfe51c6ee0e208cbc
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-06 15:11:37 +0100

    [id-123] slack integration 6

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const github = require("@actions/github");
 const slackUrl = core.getInput("slack-url");
 const message = core.getInput("message");
 
+console.log(slackUrl, message, github.context.payload.pull_request.head.ref);
+
 try {
   axios.post(slackUrl, {
     text: `[PR ${github.context.payload.pull_request.head.ref}]: ${message}`
