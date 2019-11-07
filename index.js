e74d5f0201e51213fc0a2604139725030ab036dd
Gustav Jorlöv <gustav.jorlov@gmail.com>
2019-11-07 10:02:26 +0100

    removed complexity

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const axios = require("axios");
 const core = require("@actions/core");
-const github = require("@actions/github");
 
 const slackUrl = core.getInput("slack-url");
 const message = core.getInput("message");
 
-console.log(slackUrl, message, github.context.payload.pull_request.head.ref);
-
 try {
-  axios.post(slackUrl, {
-    text: `[PR ${github.context.payload.pull_request.head.ref}]: ${message}`
-  });
+  axios.post(slackUrl, { text: message });
 } catch (error) {
   core.setFailed(error.message);
 }
